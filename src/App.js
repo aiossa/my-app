fd627df03a6ffd3c55d490ecbbad0ccf95596651
Aleksandr Iossa <iossa92@gmail.com>
2018-10-12 16:53:54 +0300

    task preparation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {simpleCalculation} from './utils/utils';
+import {simpleCalculation, compareTwoNumbers} from './utils/utils';
 
 
 class App extends Component {
   
+  constructor(props) {
+    super(props);
+    this.state = {maxNumber:0};
+  }
+
+  handleButtonPress(inp1, inp2) {
+    
+    this.setState({
+      maxNumber: compareTwoNumbers(inp1, inp2)
+    })
+  }
 
   render() {
     return (
@@ -14,12 +25,24 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React!!!</h1>
         </header>
+        <div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
             Result of calculation is {simpleCalculation(10,5)}
           </p>
+          </div>
+          <div>
+            <input id = "inp1"></input>
+            <input id = "inp2"></input>
+            <button onClick={() => this.handleButtonPress(document.getElementById("inp1").value,
+            document.getElementById("inp2").value
+            )} >Сравни числа!</button>          
+          </div>
+          <p> Максимальное число {this.state.maxNumber}
+          </p>
+          
       </div>
     );
   }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,3 +3,8 @@ export let simpleCalculation = function simpleCalculation(a, b) {
         return 'Одно из значений или оба не числа'
     }  else return a + b;
 }
+
+export let compareTwoNumbers = function compareTwoNumbers(inp1, inp2) {
+    if (Math.abs(inp1)> Math.abs(inp2)) return inp1
+    else return inp2
+}
