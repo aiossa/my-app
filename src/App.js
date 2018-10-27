7d2bb2ff325f0d93a7b7fbe7a756c141e9915949
@MilyaushaV <testsw390@gmail.com>
2018-10-27 15:26:06 +0300

    added file Milyausha.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {simpleCalculation} from './utils/utils';
-
+import Milyausha from './names/Milyausha'
 
 class App extends Component {
   
@@ -17,6 +17,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p class={Milyausha}></p>
         <p>
             Result of calculation is {simpleCalculation(10,5)}
           </p>
---
+++ src/names/Milyausha.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import {simpleCalculation} from './utils/utils'
+
+
+class Milyausha extends Component {
+  
+
+  render() {
+    return (
+      <div className="Milyausha">
+          <h1 className="App-title">Welcome to Milyausha!!!</h1>
+    
+      </div>
+    );
+  }
+}
+
+export default Milyausha;
