fa3be5a09269204f726dbeadf67f5d5e9273d448
Jane <yanafinl@gmail>
2018-10-27 15:26:06 +0300

    QA-course-456

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {simpleCalculation} from './utils/utils';
+import Jane from './names/jane';
+
 
 
 class App extends Component {
@@ -14,6 +16,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React!!!</h1>
         </header>
+        <Jane/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
