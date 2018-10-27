9b24a40a05003886dd7dcacc914f104942147e74
AIsmag <albina71994@yandex.ru>
2018-10-27 15:28:02 +0300

    name added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Aismag from './names/aismag';
 import {simpleCalculation} from './utils/utils';
 
 
@@ -12,8 +13,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!!!</h1>
+          <h1 className="App-title">Welcome to Albina1!!!</h1>
         </header>
+        
+        <Aismag/>
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
