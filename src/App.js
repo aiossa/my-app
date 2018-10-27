51efa5c25335e04b69f5ad89841661292be71fbb
Anton <antonkhvdv@gmail.com>
2018-10-27 16:29:07 +0300

    Fix merge 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Vitaliy from './vitaliy'
 import Albina_Gurova from  './names/gurova';
 import Aiossa from './names/Aiossa'
 import {simpleCalculation} from './utils/utils';
-<<<<<<< HEAD
-import Apesegov from './names/apesegov';
-
-=======
 import Mchukmarov from './names/mchukmarov';
 import Milyausha from './names/Milyausha'
->>>>>>> origin/master
 
 class App extends Component {
   
@@ -22,17 +18,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React!!!</h1>
         </header>
-<<<<<<< HEAD
-        <Apesegov/>
-=======
         The group list:
         <Aiossa/>
         <Mchukmarov/>
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
         <Albina_Gurova/>
->>>>>>> origin/master
+        <Vitaliy/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
