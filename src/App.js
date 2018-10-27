8e7f2851c11da6b62a8d3a3d6d70822cd39ee555
mchukmarov <mike.mentor@mail.ru>
2018-10-27 15:12:56 +0300

    add name mchukmarov

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {simpleCalculation} from './utils/utils';
-
+import Mchukmarov from './names/mchukmarov';
 
 class App extends Component {
   
@@ -14,6 +14,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React!!!</h1>
         </header>
+        <Mchukmarov/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
