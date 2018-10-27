672c3c9529d633497768d7135ce2e07522342797
Anton <antonkhvdv@gmail.com>
2018-10-27 15:19:41 +0300

    Fix spelling error

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!!</h1>
+          <h1 className="App-title">Welcome to React!!!</h1>
         </header>
         <Apesegov/>
         <p className="App-intro">
