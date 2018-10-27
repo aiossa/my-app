5826d41d5326fd4d52ea2c16abda1dc65b5aad87
Aleksandr <aiossa@users.noreply.github.com>
2018-10-27 16:44:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,10 +21,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React!!!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          </p>        
+        </header>       
          The group list:
         <Aiossa/>
         <Mchukmarov/>
