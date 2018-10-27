961e81ecabe24e2aded2bdab49393a0ec17fab0e
alia <ganievaalia94@gmail.com>
2018-10-27 16:18:34 +0300

    'aganieva'

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React infotech!!!</h1>
+          <h1 className="App-title">Welcome to React!!!</h1>
         </header>
         The group list:
         <Aiossa/>
