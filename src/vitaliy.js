17073fa098eb5f8df2878e66b8546175b24cc1be
Vitaliy <ilkov0501@mail.ru>
2018-10-27 15:38:59 +0300

    QACOURSE-123 Vitaliy add

---
+++ src/vitaliy.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Vitaliy extends Component {
+  
+
+  render() {
+    return (
+      <div className="Name">
+      Hello World, Vitaliy
+      </div>
+    );
+  }
+}
+
+export default Vitaliy;
