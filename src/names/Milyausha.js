eec5510f6ae88599fac98ec30a5249ec81cec700
@MilyaushaV <testsw390@gmail.com>
2018-10-27 15:40:58 +0300

    changed file Milyausha

--- src/names/Milyausha.js
+++ src/names/Milyausha.js
@@ -10,7 +10,7 @@ class Milyausha extends Component {
   render() {
     return (
       <div className="Milyausha">
-          <h1 className="App-title">Welcome to Milyausha!!!</h1>
+          <h1 className="App-title">Welcome to Milyausha!</h1>
     
       </div>
     );
