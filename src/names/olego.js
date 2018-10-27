7bbfc4efbb50963e7dcf57bcae07f26b0a2beeec
oleggaiver <oleggaiver@gmail.com>
2018-10-27 15:27:24 +0300

    QACOURSE 455 name added@

--- src/names/olego.js
+++ src/names/olego.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import olego'./names/olego.js';
-import './App.css';
-import {simpleCalculation} from './utils/utils';
+import olego './App.css';
+import {simpleCalculation} from '../utils/utils';
 
 
 class App extends Component {
@@ -12,15 +11,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!!!</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-          </header>
-          Group:        
-        <olego/>
-            <p>
+        </p>
+        <p>
             Result of calculation is {simpleCalculation(10,5)}
           </p>
       </div>
