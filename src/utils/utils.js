c963999cc972a214ae92332cef24f234e1e0266b
alia <ganievaalia94@gmail.com>
2018-11-03 10:22:15 +0300

    space

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,4 +2,4 @@ export let simpleCalculation = function simpleCalculation(a, b) {
     if (isNaN(a) || isNaN(b)) {
         return 'Одно из значений или оба не числа'
     }  else return a + b;
-}
+} 
