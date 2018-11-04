b4f9bd3bae2bffdecf15c8912d0dfb4d5e5548f7
mchukmarov <mike.mentor@mail.ru>
2018-11-04 16:24:33 +0300

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {simpleCalculation} from './utils/utils';
-
+import {compareTwoNumbers} from './utils/utils';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -10,6 +10,14 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-// it('calculation works', () => {
-//     expect(simpleCalculation(4,10)).toEqual(14);
-// });
+it('calculation works', () => {
+ expect(simpleCalculation(4,10)).toEqual(14);
+ });
+
+ it('calculation works', () => {
+  expect(compareTwoNumbers(10,9)).toEqual(10);
+  expect(compareTwoNumbers(5,17)).toEqual(17); 
+  expect(compareTwoNumbers(0,-1)).toEqual(0);
+  expect(compareTwoNumbers(5,g)).toEqual(5);  
+  });
+  
